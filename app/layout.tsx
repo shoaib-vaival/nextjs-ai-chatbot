9e79c1970ae3e6f034af671df11aa6e1917ab064
Shoaib <shoaib3430@gmail.com>
2024-10-23 08:59:06 +0000

    clean code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
-import { Metadata } from "next";
 import { Toaster } from "sonner";
 
 import { Navbar } from "@/components/custom/navbar";
 import { ThemeProvider } from "@/components/custom/theme-provider";
 
 import "./globals.css";
-
-export const metadata: Metadata = {
-  metadataBase: new URL("https://chat.vercel.ai"),
-  title: "AI Chatbot",
-  description: "AI chatbot template using the AI SDK.",
-};
 
 export default async function RootLayout({
   children,
