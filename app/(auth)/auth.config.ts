0ab0aef41d7e1b0e970b740b8c1654955bc24e4a
Shoaib <shoaib3430@gmail.com>
2024-10-24 11:42:52 +0000

    Iframe login issue

--- app/(auth)/auth.config.ts
+++ app/(auth)/auth.config.ts
@@ -36,4 +36,32 @@ export const authConfig = {
       return true;
     },
   },
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: true,
+      },
+    },
+    callbackUrl: {
+      name: `__Secure-next-auth.callback-url`,
+      options: {
+        sameSite: "lax",
+        path: "/",
+        secure: true,
+      },
+    },
+    csrfToken: {
+      name: `__Host-next-auth.csrf-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: true,
+      },
+    },
+  },
 } satisfies NextAuthConfig;
